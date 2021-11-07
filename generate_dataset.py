50e7f8770e5732f79a3586358f5863f35009c385
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2021-11-07 13:57:39 +0100

    this should not be as ram intensive
    bro look at that indent level tho @xiboon

--- generate_dataset.py
+++ generate_dataset.py
@@ -21,28 +21,32 @@ load_dotenv()
 async def fetch(ctx, user: discord.Member, gpt=""):
     if gpt and gpt != "gpt":
         return await ctx.send(f"Usage: {ctx.prefix}fetch <user> [gpt]\n<> is required, [] is optional")
+    if os.path.isfile("dataset.txt"):
+        os.remove("dataset.txt")
+    if os.path.isfile("gpt_dataset.txt"):
+        os.remove("gpt_dataset.txt")
     msg = await ctx.send("Fetching messages...\nThis will take a while.")
-    messages = []
     count = 0
+    name = None
     for channel in [channel for channel in ctx.guild.channels if isinstance(channel, discord.TextChannel)]:
         try:
             async for message in channel.history(limit=None):
                 count += 1
                 print(f"Processed {count} messages")
                 if message.author.id == user.id:
-                    messages.append(message)
+                    if not gpt:
+                        name = "dataset.txt"
+                        with open(name, "a", encoding="utf-8") as f:
+                            f.write(f"{message.content}\n")
+                    else:
+                        name = "gpt_dataset.txt"
+                        with open(name, "a", encoding="utf-8") as f:
+                            f.write(f"<|startoftext|> {message.content} <|endoftext|>\n")
         except discord.Forbidden:
             pass
-    if not gpt:
-        res = "\n".join(message.content for message in messages)
-        name = "dataset.txt"
-    else:
-        res = "\n".join(f"<|startoftext|> {message.content} <|endoftext|>" for message in messages)
-        name = "gpt_dataset.txt"
-    with open(name, "w", encoding="utf-8") as f:
-        f.write(res)
     await msg.delete()
     await ctx.send(file=discord.File(name))
+
 
 
 @client.event
